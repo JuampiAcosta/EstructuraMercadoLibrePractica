d69006e73a5ba67098967179a831ab92c7891200
JuampiAcosta <acostajuan678@gmail.com>
2022-08-22 20:55:59 -0300

    Se le agrego el archivo login y registro en formato html
    Se le agrego el archivo login y registro en formato html le falta mejorar varios puntos

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const path = require('path');
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'./views/home.html'));
 });
+app.get('/login.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
 
 // Se levanta el servidor
 app.listen( 3000,console.log("El Servidor esta funcionando"));
