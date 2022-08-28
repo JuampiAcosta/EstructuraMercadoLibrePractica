ebc0bab7abef49ab33b8fefd94f537a580c8d99b
JuampiAcosta <acostajuan678@gmail.com>
2022-08-27 21:09:13 -0300

    Haciendo varios cambios. Todavia no se termino

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 
 const express = require('express');
 const app = express();
-
 app.use(express.static('public'));
 
-const path = require('path');
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en http://localhost:3000');
+});
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/home.html'));
-});
-app.get('/login.html', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/login.html'));
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-// Se levanta el servidor
-app.listen( 3000,console.log("El Servidor esta funcionando"));
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+
+
+
+
+
